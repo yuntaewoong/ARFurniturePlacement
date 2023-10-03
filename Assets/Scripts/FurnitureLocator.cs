5a590448d7d945a95e456141f494e943551c713e
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-10-03 16:05:15 +0900

    Add Feature "Placing Furniture On Locating Point"

---
+++ Assets/Scripts/FurnitureLocator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureLocator : MonoBehaviour
+{
+    public GameObject[] Furnitures;//배치할 가구 Prefab 배열
+    private int CurrentFurnitureIndex = 0;//배치할 가구의 Index
+    private LocationPointingIndicator LocationIndicator;
+
+    private void Start()
+    {
+        LocationIndicator = FindObjectOfType<LocationPointingIndicator>();
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {//터치 발생 시 
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {//터치 시작할때
+                TouchEvent();
+            }
+        }
+    }
+    private void TouchEvent()
+    {
+        Vector3 furniturePosition = LocationIndicator.GetIndicatorPosition();
+        Quaternion furnitureRotation = LocationIndicator.GetIndicatorRotation();
+        Instantiate(Furnitures[CurrentFurnitureIndex], furniturePosition, furnitureRotation);//배치하기
+    }
+
+}
--- Assets/Scripts/LocationPointingIndicator.cs
+++ Assets/Scripts/LocationPointingIndicator.cs
@@ -5,33 +5,44 @@ using UnityEngine.XR.ARFoundation;
 
 public class LocationPointingIndicator : MonoBehaviour
 {
-    private ARRaycastManager raycastManager;
-    private GameObject pointerImageObject;
+    private ARRaycastManager RaycastManager;
+    private GameObject PointerImageObject;
+
+
+    public Vector3 GetIndicatorPosition()
+    {
+        return transform.position;
+    }
+    public Quaternion GetIndicatorRotation()
+    {
+        return transform.rotation;
+    }
+
 
     private void Start()
     {
-        raycastManager = FindObjectOfType<ARRaycastManager>();
+        RaycastManager = FindObjectOfType<ARRaycastManager>();
         if(transform.childCount == 0)
         {
             Debug.LogError("LocationPointingIndicator Must be have child");
             return;
         }
-        pointerImageObject = transform.GetChild(0).gameObject;
-        pointerImageObject.SetActive(false);
+        PointerImageObject = transform.GetChild(0).gameObject;
+        PointerImageObject.SetActive(false);
     }
     private void Update()
     {
         List<ARRaycastHit> raycastHitList = new List<ARRaycastHit>();
-        raycastManager.Raycast(
+        RaycastManager.Raycast(
             new Vector2(Screen.width / 2, Screen.height / 2),
             raycastHitList, 
             UnityEngine.XR.ARSubsystems.TrackableType.Planes
-        );//AR Plane과 충돌 처리
+        );//AR Plane과 충돌 처리(Screen 중앙 -> AR Plane 검출)
         if(raycastHitList.Count > 0)
         {
-            if(!pointerImageObject.activeInHierarchy)
+            if(!PointerImageObject.activeInHierarchy)
             {
-                pointerImageObject.SetActive(true);
+                PointerImageObject.SetActive(true);
             }
             transform.position = raycastHitList[0].pose.position;
             transform.rotation = raycastHitList[0].pose.rotation;
