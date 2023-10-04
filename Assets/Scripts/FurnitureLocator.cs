ce3996cabb0c0164ab83d708f5a81aa7dbb11018
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2023-10-04 18:11:03 +0900

    Add feature "Toggle Off When Touching"

--- Assets/Scripts/FurnitureLocator.cs
+++ Assets/Scripts/FurnitureLocator.cs
@@ -6,7 +6,6 @@ using UnityEngine.XR.ARFoundation;
 public class FurnitureLocator : MonoBehaviour
 {
     public GameObject[] Furnitures;//배치할 가구 Prefab 배열
-    private int CurrentFurnitureIndex = 0;//배치할 가구의 Index
     private LocationPointingIndicator LocationIndicator;
 
     private void Start()
@@ -21,15 +20,27 @@ public class FurnitureLocator : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             if(touch.phase == TouchPhase.Began)
             {//터치 시작할때
-                TouchEvent();
+                if(touch.position.y > 200)
+                {
+                    TouchEvent();
+                    UIManager.Instance.SetAnyToggleSelected(false);
+                }
             }
         }
     }
     private void TouchEvent()
     {
+        if(!UIManager.Instance.IsAnyToggleSelected())
+        {//모든 토글이 꺼져잇다면 아무것도 하지 않는다
+            return;
+        }
         Vector3 furniturePosition = LocationIndicator.GetIndicatorPosition();
         Quaternion furnitureRotation = LocationIndicator.GetIndicatorRotation();
-        GameObject furniture = Instantiate(Furnitures[CurrentFurnitureIndex], furniturePosition, furnitureRotation);//배치하기
+        GameObject furniture = Instantiate(
+            Furnitures[UIManager.Instance.GetCurrentFurnitureIndex()],
+            furniturePosition, 
+            furnitureRotation
+        );//배치하기
         furniture.AddComponent<ARAnchor>();
     }
 
